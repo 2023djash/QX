5250082ff63ca4273aeb5b674cf090d4c4fb2683
石七画 <141410871+2023djash@users.noreply.github.com>
2023-11-28 23:16:29 +0800

    更新 Nicegram.js

--- Nicegram.js
+++ Nicegram.js
@@ -3,15 +3,35 @@
 Nicegram 1.4.7
 
 [rewrite_local]
-https://nicegram.cloud/api/v6/user/info url script-response-body https://raw.githubusercontent.com/2023djash/QX/main/Nicegram.js
+^https?:\/\/(nicegram\.cloud\/api\/v\d\/user\/info|restore-access\.indream\.app\/restoreAccess) url script-response-body https://raw.githubusercontent.com/2023djash/QX/main/Nicegram.js
 
-[mitm] 
-hostname = nicegram.cloud
+[mitm]
+hostname = nicegram.cloud, restore-access.indream.app
 
-**/
+*************************************/
 
-var Q = JSON.parse($response.body);
-Q.data.user.lifetime_subscription = true;
-Q.data.user.store_subscription = true;
-Q.data.user.subscription = true;
-$done({body : JSON.stringify(Q)});
+
+const url = $request.url;
+const isQX = typeof $task !== "undefined";
+var chxm1023 = JSON.parse($response.body);
+const subscriptionTest = /nicegram\.cloud\/api\/v\d\/user\/info/;
+const premiumTest = /restoreAccess/;
+
+if (subscriptionTest.test(url)) {
+  chxm1023.data.user = {
+    ...chxm1023.data.user,
+    subscription: true,
+    store_subscription: true,
+    lifetime_subscription: true
+  };
+}
+
+if (premiumTest.test(url)) {
+  chxm1023 = {"data":{"premiumAccess": true}};
+}
+
+function finalizeResponse(content) {
+  return { status: isQX ? "HTTP/1.1 200 OK" : 200, headers: $response.headers, body: JSON.stringify(content) };
+}
+
+$done(isQX ? finalizeResponse(chxm1023) : chxm1023);
